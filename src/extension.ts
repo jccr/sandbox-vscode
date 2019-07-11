81cb821d7278c2f2b7c98d89d12e4eda2c7e1200
Juan Carlos Corona Romero <c@rlos.email>
2019-07-10 22:57:34 -0700

    Show editors in 2x2 grid

--- src/extension.ts
+++ src/extension.ts
@@ -15,36 +15,72 @@ export function activate(context: vscode.ExtensionContext) {
       isCaseSensitive: true
     })
   );
-
+  
   // The command has been defined in the package.json file
   // Now provide the implementation of the command with registerCommand
   // The commandId parameter must match the command field in package.json
   let disposable = vscode.commands.registerCommand(
     "extension.openSandbox",
     async () => {
-      // The code you place here will be executed every time your command is executed
-      vscode.workspace.updateWorkspaceFolders(0, 0, {
-        uri: vscode.Uri.parse("sandbox:/"),
-        name: "Sandbox"
+      vscode.workspace.updateWorkspaceFolders(
+        vscode.workspace.workspaceFolders
+          ? vscode.workspace.workspaceFolders.length
+          : 0,
+        null,
+        {
+          uri: vscode.Uri.parse("sandbox:/"),
+          name: "Sandbox"
+        }
+      );
+      const htmlUri = vscode.Uri.parse(`sandbox:/index.html`);
+      const cssUri = vscode.Uri.parse(`sandbox:/style.css`);
+      const jsUri = vscode.Uri.parse(`sandbox:/script.js`);
+      memFs.writeFile(htmlUri, new Uint8Array(0), {
+        create: true,
+        overwrite: true
+      });
+      memFs.writeFile(cssUri, new Uint8Array(0), {
+        create: true,
+        overwrite: true
+      });
+      memFs.writeFile(jsUri, new Uint8Array(0), {
+        create: true,
+        overwrite: true
+      });
+      // // The code you place here will be executed every time your command is executed
+      // vscode.workspace.updateWorkspaceFolders(0, 0, {
+      //   uri: vscode.Uri.parse("sandbox:/"),
+      //   name: "Sandbox"
+      // });
+
+      const htmlTextDocument = await vscode.workspace.openTextDocument(htmlUri);
+      const cssTextDocument = await vscode.workspace.openTextDocument(cssUri);
+      const jsTextDocument = await vscode.workspace.openTextDocument(jsUri);
+      await vscode.commands.executeCommand("vscode.setEditorLayout", {
+        groups: [
+          { groups: [{}, {}], size: 0.5 },
+          { groups: [{}, {}], size: 0.5 }
+        ]
+      });
+      await vscode.window.showTextDocument(htmlTextDocument, {
+        preview: false,
+        viewColumn: vscode.ViewColumn.One
+      });
+      await vscode.window.showTextDocument(cssTextDocument, {
+        preview: false,
+        viewColumn: vscode.ViewColumn.Two
+      });
+      await vscode.window.showTextDocument(jsTextDocument, {
+        preview: false,
+        viewColumn: vscode.ViewColumn.Three
       });
 
-      vscode.workspace.onDidChangeWorkspaceFolders(async event => {
-        console.log(event);
-
-        memFs.writeFile(
-          vscode.Uri.parse(`sandbox:/file.txt`),
-          Buffer.from("foo"),
-          {
-            create: true,
-            overwrite: true
-          }
-        );
-
-        let doc = await vscode.workspace.openTextDocument(
-          vscode.Uri.parse(`sandbox:/file.txt`)
-        ); // calls back into the provider
-        await vscode.window.showTextDocument(doc, { preview: false });
-      });
+      const panel = vscode.window.createWebviewPanel(
+        "sandboxResult",
+        "Result",
+        vscode.ViewColumn.Four,
+        {}
+      );
     }
   );
 
